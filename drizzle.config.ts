7c666e60545192379d05c87a96371f806cdd9d2f
shaon07 <alishaon078@gmail.com>
2026-06-02 21:12:08 +0600

    feat: update database configuration to use environment variables for auth token; add production environment file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import { config } from "dotenv";
+
+config({ path: ".env.local" });
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./src/db/migrations",
   dialect: "sqlite",
   dbCredentials: {
-    url: process.env.DATABASE_URL || "file:local.db",
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 });
